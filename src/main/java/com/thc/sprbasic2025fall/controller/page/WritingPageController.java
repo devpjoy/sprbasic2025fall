e6052621626f6baca21b49576f0e46a9a6892dbe
devpjoy <devpjoy@gmail.com>
2025-09-16 21:08:17 +0900

    0916 finished

---
+++ src/main/java/com/thc/sprbasic2025fall/controller/page/WritingPageController.java
@@ -0,0 +1,23 @@
+package com.thc.sprbasic2025fall.controller.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/writing/")
+@Controller
+public class WritingPageController {
+
+    //@RequestMapping("/create")
+    @GetMapping("/{page}") // Get으로만 접근할 수 있는 RequestMapping (기능은 동일)
+    public String create(@PathVariable String page) {
+        return "writing/" + page;
+    }
+
+    @GetMapping("/{page}/{id}")
+    public String page2(@PathVariable String page, @PathVariable String id) {
+        // @PathVariable 여러 개 사용 가능
+        return "writing/" + page;
+    }
+}
